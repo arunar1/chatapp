aa7a56fa7a74e9b96e972c4a52dd7fbe39f3202a
arunar1 <arunarofficial00@gmail.com>
2023-07-17 11:48:58 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router,Routes,Route, Switch,Link } from 'react-router-dom';
 
+import LoginPage from './components/LoginComponent/LoginPage';
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Route path='/'>
+          <LoginPage/>
+        </Route>
+      </Router>
     </div>
   );
 }
--- src/components/LoginComponent/LoginPage.js
+++ src/components/LoginComponent/LoginPage.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import './LoginPage.css'
+import Header from './Header'
+import Main from './Main'
+import Footer from './Footer'
 export default function LoginPage() {
   return (
     <div>
-      
+      <Header/>
+      <Main/>
+      <Footer/>
     </div>
   )
 }
