cdcbabc3b9255a98544c6b80d403c157cadcd4de
arunar1 <arunarofficial00@gmail.com>
2023-07-17 21:16:43 +0530

    commit

--- src/components/LoginComponent/Main.js
+++ src/components/LoginComponent/Main.js
@@ -4,7 +4,24 @@ export default function Main() {
   return (
     <div>
       <div className='main'>
-        main
+      <div class="right-section">
+                    <h4>Hello,</h4>
+                    <form action="" autocomplete="off">
+                        <div class="input-group">
+                          <input type="text" id="name" required />  
+                          <label for="name">Name</label>
+                        </div>
+                        <div class="input-group">
+                            <input type="number" id="emai" required />
+                            <label for="emai">Room No</label>
+                        </div>
+                        
+                        <div class="submit-btn">
+                            <input type="submit" class="submit" />
+                        </div>
+                    </form>
+
+                </div>
       </div>
     </div>
   )
